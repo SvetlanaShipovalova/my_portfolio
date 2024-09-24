12ce33dcd96f33a11a4d1a3d7ee1b8847c6d196a
SvetlanaShipovalova <90263099+SvetlanaShipovalova@users.noreply.github.com>
2024-09-24 15:28:31 +0300

    .

---
+++ sketch.js
@@ -0,0 +1,62 @@
+function setup() {
+    var canvas = createCanvas(120, 1340);
+    canvas.parent('canvasForHTML');
+	
+}
+
+function draw() {
+
+    strokeWeight(4);
+    stroke(188, 62, 78);
+    noFill();
+    beginShape(LINES);
+    vertex(24, 4);
+    vertex(50, 56);
+	
+    vertex(50, 56);
+    vertex(96, 85);
+	
+    vertex(96, 85);
+    vertex(4, 167);
+	
+    vertex(4, 167);
+    vertex(107, 444);
+	
+    vertex(107, 444);
+    vertex(4, 560);
+	
+	vertex(4, 560);
+	vertex(34, 600);
+	
+	vertex(34, 600);
+	vertex(90, 700);
+	
+	vertex(90, 700);
+	vertex(113, 900);
+	
+	vertex(113, 900);
+	vertex(40, 950);
+	
+	vertex(40, 950);
+	vertex(7, 980);
+	
+	vertex(7, 980);
+	vertex(45, 1060);
+	
+	vertex(45, 1060);
+	vertex(35, 1080);
+	
+	vertex(35, 1080);
+	vertex(116, 1200);
+	
+	vertex(116, 1200);
+	vertex(25, 1290);
+	
+	vertex(25, 1290);
+	vertex(2, 1340);
+	
+	vertex(2, 1340);
+	vertex(117, 1450);
+    endShape();
+
+}
